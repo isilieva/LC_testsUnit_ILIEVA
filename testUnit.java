1ba0d60f875706e4b561defa4619d07ea7d84013
isilieva <105059287+isilieva@users.noreply.github.com>
2022-08-03 10:03:59 +0200

    feat(java): Tp2 update

--- testUnit.java
+++ testUnit.java
@@ -1,35 +1,37 @@
 import user.Personne;
 import junit.framework.TestCase; 
 
+@Test
 public class userTest extends TestCase
 { 
     private static Personne user = new Personne();
 
-#test Email
-
-    public void testEmail()
+/* #test Email */
+@Test
+public void testEmail()
     { 
          email = "username@domain.com";
          assertTrue(EmailValidator.getEmail()
             .isValid(email));
     }
 
-#test Nom 
-
+/*test Nom*/
+@Test
   public void testGetNom() {
-    assertEquals("Le nom est incorrect", "nom", personne.getNom());
+    assertEquals("Le nom est incorrect", "nom", user.getNom());
   }
 
-#test Prenom 
-
+/*#test Prenom*/
+ 
+@Test
 public void testGetPrenom() {
-    assertEquals("Le prenom est incorrect", "prenom", personne.getPrenom());
+    assertEquals("Le prenom est incorrect", "prenom", user.getPrenom());
   }
 
-#test Age
+/* #test Age */
 
-
+@Test
         public void testIfAgeIsGreaterThan(){
-            assertTrue(personne.getAge()>30);
+            assertTrue(user.getAge()>30);
         }
 }
